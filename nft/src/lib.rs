c3db98710b1f46f02972265240568976565063ba
dariacherniakk <dc@binary-star.org>
2023-01-04 19:21:54 +0000

    Remove unnecessary files, add token_owners field

--- nft/src/lib.rs
+++ nft/src/lib.rs
@@ -15,11 +15,12 @@ NOTES:
   - To prevent the deployed contract from being modified or deleted, it should not have any access
     keys on its account.
 */
+
 use near_contract_standards::non_fungible_token::metadata::{
     NFTContractMetadata, NonFungibleTokenMetadataProvider, TokenMetadata, NFT_METADATA_SPEC,
 };
-use near_contract_standards::non_fungible_token::{Token, TokenId};
 use near_contract_standards::non_fungible_token::NonFungibleToken;
+use near_contract_standards::non_fungible_token::{Token, TokenId};
 use near_sdk::borsh::{self, BorshDeserialize, BorshSerialize};
 use near_sdk::collections::LazyOption;
 use near_sdk::{
@@ -33,7 +34,12 @@ pub struct Contract {
     metadata: LazyOption<NFTContractMetadata>,
 }
 
+// TODO
 const DATA_IMAGE_SVG_NEAR_ICON: &str = "data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 288 288'%3E%3Cg id='l' data-name='l'%3E%3Cpath d='M187.58,79.81l-30.1,44.69a3.2,3.2,0,0,0,4.75,4.2L191.86,103a1.2,1.2,0,0,1,2,.91v80.46a1.2,1.2,0,0,1-2.12.77L102.18,77.93A15.35,15.35,0,0,0,90.47,72.5H87.34A15.34,15.34,0,0,0,72,87.84V201.16A15.34,15.34,0,0,0,87.34,216.5h0a15.35,15.35,0,0,0,13.08-7.31l30.1-44.69a3.2,3.2,0,0,0-4.75-4.2L96.14,186a1.2,1.2,0,0,1-2-.91V104.61a1.2,1.2,0,0,1,2.12-.77l89.55,107.23a15.35,15.35,0,0,0,11.71,5.43h3.13A15.34,15.34,0,0,0,216,201.16V87.84A15.34,15.34,0,0,0,200.66,72.5h0A15.35,15.35,0,0,0,187.58,79.81Z'/%3E%3C/g%3E%3C/svg%3E";
+const NFT_NAME: &str = "Tonic Greedy Goblins";
+const NFT_SYMBOL: &str = "GGB";
+const BASE_IRI: &str =
+    "https://enleap.infura-ipfs.io/ipfs/QmPUtd7VLoy1Ursa9w4gKwL12W9tMxJXvHkMEZ2VZrPnem";
 
 #[derive(BorshSerialize, BorshStorageKey)]
 enum StorageKey {
@@ -54,10 +60,10 @@ impl Contract {
             owner_id,
             NFTContractMetadata {
                 spec: NFT_METADATA_SPEC.to_string(),
-                name: "Example NEAR non-fungible token".to_string(),
-                symbol: "EXAMPLE".to_string(),
+                name: NFT_NAME.to_string(),
+                symbol: NFT_SYMBOL.to_string(),
                 icon: Some(DATA_IMAGE_SVG_NEAR_ICON.to_string()),
-                base_uri: None,
+                base_uri: Some(BASE_IRI.to_string()),
                 reference: None,
                 reference_hash: None,
             },
@@ -95,7 +101,8 @@ impl Contract {
         receiver_id: AccountId,
         token_metadata: TokenMetadata,
     ) -> Token {
-        self.tokens.mint(token_id, receiver_id, Some(token_metadata))
+        self.tokens
+            .internal_mint(token_id, receiver_id, Some(token_metadata))
     }
 }
 
