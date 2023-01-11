da44a458d3f0f77c24bf713b5ed0c5dd43af61f2
dariacherniakk <dc@binary-star.org>
2023-01-11 22:54:57 +0000

    Add methods to synchronize DAO members and NFT owners

---
+++ dao/src/dao_impl.rs
@@ -0,0 +1,80 @@
+use std::collections::{HashSet, HashMap};
+
+use near_sdk::{serde::Deserialize, json_types::{U128, U64}, AccountId};
+
+#[derive(Deserialize)]
+#[serde(crate = "near_sdk::serde")]
+pub struct Policy {
+    /// List of roles and permissions for them in the current policy.
+    pub roles: Vec<RolePermission>,
+    /// Default vote policy. Used when given proposal kind doesn't have special policy.
+    pub default_vote_policy: VotePolicy,
+    /// Proposal bond.
+    pub proposal_bond: U128,
+    /// Expiration period for proposals.
+    pub proposal_period: U64,
+    /// Bond for claiming a bounty.
+    pub bounty_bond: U128,
+    /// Period in which giving up on bounty is not punished.
+    pub bounty_forgiveness_period: U64,
+}
+
+#[derive(Deserialize)]
+#[serde(crate = "near_sdk::serde")]
+pub struct RolePermission {
+    /// Name of the role to display to the user.
+    pub name: String,
+    /// Kind of the role: defines which users this permissions apply.
+    pub kind: RoleKind,
+    /// Set of actions on which proposals that this role is allowed to execute.
+    /// <proposal_kind>:<action>
+    pub permissions: HashSet<String>,
+    /// For each proposal kind, defines voting policy.
+    pub vote_policy: HashMap<String, VotePolicy>,
+}
+
+/// Defines configuration of the vote.
+#[derive(Deserialize)]
+#[serde(crate = "near_sdk::serde")]
+pub struct VotePolicy {
+    /// Kind of weight to use for votes.
+    pub weight_kind: WeightKind,
+    /// Minimum number required for vote to finalize.
+    /// If weight kind is TokenWeight - this is minimum number of tokens required.
+    ///     This allows to avoid situation where the number of staked tokens from total supply is too small.
+    /// If RoleWeight - this is minimum number of votes.
+    ///     This allows to avoid situation where the role is got too small but policy kept at 1/2, for example.
+    pub quorum: U128,
+    /// How many votes to pass this vote.
+    pub threshold: WeightOrRatio,
+}
+
+#[derive(Deserialize)]
+#[serde(crate = "near_sdk::serde")]
+pub enum RoleKind {
+    /// Matches everyone, who is not matched by other roles.
+    Everyone,
+    /// Member greater or equal than given balance. Can use `1` as non-zero balance.
+    Member(U128),
+    /// Set of accounts.
+    Group(HashSet<AccountId>),
+}
+
+/// Direct weight or ratio to total weight, used for the voting policy.
+#[derive(Deserialize)]
+#[serde(crate = "near_sdk::serde")]
+#[serde(untagged)]
+pub enum WeightOrRatio {
+    Weight(U128),
+    Ratio(u64, u64),
+}
+
+/// How the voting policy votes get weigthed.
+#[derive(Deserialize)]
+#[serde(crate = "near_sdk::serde")]
+pub enum WeightKind {
+    /// Using token amounts and total delegated at the moment.
+    TokenWeight,
+    /// Weight of the group role. Roles that don't have scoped group are not supported.
+    RoleWeight,
+}
--- dao/src/lib.rs
+++ dao/src/lib.rs
@@ -1,31 +1,50 @@
-use std::collections::HashSet;
+mod dao_impl;
+
+use dao_impl::*;
+use std::collections::{HashMap, HashSet};
 
 use near_sdk::borsh::{self, BorshDeserialize, BorshSerialize};
 use near_sdk::{env, near_bindgen, AccountId, Gas, PanicOnDefault, Promise};
 
-const TGAS_GET_NFT_TOKENS: u64 = 100;
+const TGAS_GET_NFT_TOKENS: u64 = 50;
+const TGAS_GET_DAO_POLICY: u64 = 50;
+
+#[derive(BorshDeserialize, BorshSerialize, Clone)]
+enum MembershipType {
+    Add,
+    Remove,
+}
 
 #[near_bindgen]
 #[derive(BorshDeserialize, BorshSerialize, PanicOnDefault)]
 pub struct Contract {
     owner_id: AccountId,
+
     nft_contract_id: AccountId,
     nft_owners: HashSet<AccountId>,
+
+    dao_account_id: AccountId,
+    dao_owners_role: String,
+
+    proposal_owners: HashMap<AccountId, MembershipType>,
 }
 
 #[near_bindgen]
 impl Contract {
     #[init]
-    pub fn new(owner_id: AccountId, nft_contract_id: AccountId) -> Self {
+    pub fn new(owner_id: AccountId, nft_contract_id: AccountId, dao_account_id: AccountId) -> Self {
         assert!(!env::state_exists(), "Already initialized");
         Self {
             owner_id,
             nft_contract_id,
             nft_owners: HashSet::new(),
+            dao_account_id,
+            dao_owners_role: String::new(),
+            proposal_owners: HashMap::new(),
         }
     }
 
-    /// Synchronizing NFT owners. Removes those ones who don't have NFT anymore,
+    /// Synchronize NFT owners. Removes those ones who don't have NFT anymore,
     /// insert new owners. Return `true` if NFT owners are fully synchronized.
     pub fn sync_nft_owners(&mut self) -> Promise {
         assert_eq!(self.owner_id, env::predecessor_account_id());
@@ -35,11 +54,11 @@ impl Contract {
 
         Promise::new(self.nft_contract_id.clone())
             .function_call("nft_owners".into(), vec![], 0, gas)
-            .then(ext_self.handle_owners_sync())
+            .then(ext_self.handle_nft_owners_sync())
     }
 
     #[private]
-    pub fn handle_owners_sync(&mut self, #[callback] owners: HashSet<AccountId>) -> bool {
+    pub fn handle_nft_owners_sync(&mut self, #[callback] owners: HashSet<AccountId>) -> bool {
         let owners_to_remove: Vec<AccountId> = self
             .nft_owners
             .difference(&owners)
@@ -58,6 +77,47 @@ impl Contract {
         owners.len() == self.nft_owners.len()
     }
 
+    /// Synchronize DAO members and NFT owners. Save those ones who need to be added or removed.
+    pub fn sync_dao_owners(&mut self) -> Promise {
+        assert_eq!(self.owner_id, env::predecessor_account_id());
+
+        let ext_self = Self::ext(env::current_account_id());
+        let gas = Gas::ONE_TERA * TGAS_GET_DAO_POLICY;
+
+        Promise::new(self.dao_account_id.clone())
+            .function_call("get_policy".into(), vec![], 0, gas)
+            .then(ext_self.handle_dao_owners_sync())
+    }
+
+    #[private]
+    pub fn handle_dao_owners_sync(&mut self, #[callback] policy: Policy) {
+        let role_kind = &policy
+            .roles
+            .iter()
+            .find(|role| role.name == self.dao_owners_role)
+            .expect("Role not found")
+            .kind;
+
+        let members = match role_kind {
+            RoleKind::Group(members) => members,
+            _ => env::panic_str("Wrong role kind"),
+        };
+
+        let owners_to_add: Vec<AccountId> = self
+            .nft_owners
+            .difference(&members)
+            .map(Clone::clone)
+            .collect();
+
+        let owners_to_remove: Vec<AccountId> = members
+            .difference(&self.nft_owners)
+            .map(Clone::clone)
+            .collect();
+
+        self.extend_owners_for_proposal(owners_to_add, MembershipType::Add);
+        self.extend_owners_for_proposal(owners_to_remove, MembershipType::Remove);
+    }
+
     pub fn clear_nft_owners(&mut self) -> usize {
         assert_eq!(self.owner_id, env::predecessor_account_id());
         self.nft_owners.clear();
@@ -66,6 +126,11 @@ impl Contract {
 
     pub fn get_nft_owners(&self) -> HashSet<AccountId> {
         self.nft_owners.clone()
+    }
+
+    pub fn set_dao_role(&mut self, role: String) {
+        assert_eq!(self.owner_id, env::predecessor_account_id());
+        self.dao_owners_role = role;
     }
 
     pub fn owners_len(&self) -> usize {
@@ -77,5 +142,17 @@ impl Contract {
         for user in users {
             self.nft_owners.remove(&user);
         }
+    }
+
+    fn extend_owners_for_proposal(
+        &mut self,
+        owners: Vec<AccountId>,
+        membership_type: MembershipType,
+    ) {
+        self.proposal_owners.extend(
+            owners
+                .iter()
+                .map(|owner| (owner.clone(), membership_type.clone())),
+        );
     }
 }
