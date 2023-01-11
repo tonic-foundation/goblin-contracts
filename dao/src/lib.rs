ac993da7a46a07fcb23005ef911f338140fe9f2e
dariacherniakk <dc@binary-star.org>
2023-01-11 12:29:47 +0000

    Address comments

--- dao/src/lib.rs
+++ dao/src/lib.rs
@@ -5,31 +5,22 @@ use near_sdk::{env, near_bindgen, AccountId, Gas, PanicOnDefault, Promise};
 
 const TGAS_GET_NFT_TOKENS: u64 = 100;
 
-pub fn goblins_id() -> AccountId {
-    if cfg!(feature = "mainnet") {
-        "" // TODO insert our contract address
-    } else {
-        "dev-1673361753463-54922973878649" // Test NFT contract with 2k owners
-    }
-    .parse()
-    .unwrap()
-}
-
 #[near_bindgen]
 #[derive(BorshDeserialize, BorshSerialize, PanicOnDefault)]
 pub struct Contract {
     owner_id: AccountId,
-
+    nft_contract_id: AccountId,
     nft_owners: HashSet<AccountId>,
 }
 
 #[near_bindgen]
 impl Contract {
     #[init]
-    pub fn new(owner_id: AccountId) -> Self {
+    pub fn new(owner_id: AccountId, nft_contract_id: AccountId) -> Self {
         assert!(!env::state_exists(), "Already initialized");
         Self {
             owner_id,
+            nft_contract_id,
             nft_owners: HashSet::new(),
         }
     }
@@ -42,7 +33,7 @@ impl Contract {
         let ext_self = Self::ext(env::current_account_id());
         let gas = Gas::ONE_TERA * TGAS_GET_NFT_TOKENS;
 
-        Promise::new(goblins_id())
+        Promise::new(self.nft_contract_id.clone())
             .function_call("nft_owners".into(), vec![], 0, gas)
             .then(ext_self.handle_owners_sync())
     }
@@ -55,7 +46,7 @@ impl Contract {
             .map(Clone::clone)
             .collect();
 
-        self.remove_goblins(owners_to_remove);
+        self.remove_owners(owners_to_remove);
 
         let owners_to_add: Vec<AccountId> = owners
             .difference(&self.nft_owners)
@@ -64,7 +55,7 @@ impl Contract {
 
         self.nft_owners.extend(owners_to_add);
 
-        self.nft_owners.len() == self.nft_owners.len()
+        owners.len() == self.nft_owners.len()
     }
 
     pub fn clear_nft_owners(&mut self) -> usize {
@@ -81,7 +72,7 @@ impl Contract {
         self.nft_owners.len()
     }
 
-    fn remove_goblins(&mut self, users: Vec<AccountId>) {
+    pub fn remove_owners(&mut self, users: Vec<AccountId>) {
         assert_eq!(self.owner_id, env::predecessor_account_id());
         for user in users {
             self.nft_owners.remove(&user);
--- nft/src/lib.rs
+++ nft/src/lib.rs
@@ -22,7 +22,7 @@ use std::collections::{HashMap, HashSet};
 
 use near_contract_standards::non_fungible_token::core::NonFungibleTokenCore;
 use near_contract_standards::non_fungible_token::metadata::{
-    NFTContractMetadata, NonFungibleTokenMetadataProvider, TokenMetadata, NFT_METADATA_SPEC,
+    NFTContractMetadata, NonFungibleTokenMetadataProvider, TokenMetadata,
 };
 use near_contract_standards::non_fungible_token::NonFungibleToken;
 use near_contract_standards::non_fungible_token::{Token, TokenId};
@@ -41,13 +41,6 @@ pub struct Contract {
     token_owners: HashSet<AccountId>,
 }
 
-// TODO
-const DATA_IMAGE_SVG_NEAR_ICON: &str = "data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 288 288'%3E%3Cg id='l' data-name='l'%3E%3Cpath d='M187.58,79.81l-30.1,44.69a3.2,3.2,0,0,0,4.75,4.2L191.86,103a1.2,1.2,0,0,1,2,.91v80.46a1.2,1.2,0,0,1-2.12.77L102.18,77.93A15.35,15.35,0,0,0,90.47,72.5H87.34A15.34,15.34,0,0,0,72,87.84V201.16A15.34,15.34,0,0,0,87.34,216.5h0a15.35,15.35,0,0,0,13.08-7.31l30.1-44.69a3.2,3.2,0,0,0-4.75-4.2L96.14,186a1.2,1.2,0,0,1-2-.91V104.61a1.2,1.2,0,0,1,2.12-.77l89.55,107.23a15.35,15.35,0,0,0,11.71,5.43h3.13A15.34,15.34,0,0,0,216,201.16V87.84A15.34,15.34,0,0,0,200.66,72.5h0A15.35,15.35,0,0,0,187.58,79.81Z'/%3E%3C/g%3E%3C/svg%3E";
-const NFT_NAME: &str = "Tonic Greedy Goblins";
-const NFT_SYMBOL: &str = "GGB";
-const BASE_IRI: &str =
-    "https://enleap.infura-ipfs.io/ipfs/QmPUtd7VLoy1Ursa9w4gKwL12W9tMxJXvHkMEZ2VZrPnem";
-
 #[derive(BorshSerialize, BorshStorageKey)]
 enum StorageKey {
     NonFungibleToken,
@@ -59,24 +52,6 @@ enum StorageKey {
 
 #[near_bindgen]
 impl Contract {
-    /// Initializes the contract owned by `owner_id` with
-    /// default metadata (for example purposes only).
-    #[init]
-    pub fn new_default_meta(owner_id: AccountId) -> Self {
-        Self::new(
-            owner_id,
-            NFTContractMetadata {
-                spec: NFT_METADATA_SPEC.to_string(),
-                name: NFT_NAME.to_string(),
-                symbol: NFT_SYMBOL.to_string(),
-                icon: Some(DATA_IMAGE_SVG_NEAR_ICON.to_string()),
-                base_uri: Some(BASE_IRI.to_string()),
-                reference: None,
-                reference_hash: None,
-            },
-        )
-    }
-
     #[init]
     pub fn new(owner_id: AccountId, metadata: NFTContractMetadata) -> Self {
         assert!(!env::state_exists(), "Already initialized");
@@ -185,11 +160,23 @@ mod tests {
         }
     }
 
+    fn nft_contract_metadata() -> NFTContractMetadata {
+        NFTContractMetadata {
+            spec: "nft-1.0.0".to_string(),
+            name: "Tonic Greedy Goblins".to_string(),
+            symbol: "GGB".to_string(),
+            icon: None,
+            base_uri: None,
+            reference: None,
+            reference_hash: None,
+        }
+    }
+
     #[test]
     fn test_new() {
         let mut context = get_context(accounts(1));
         testing_env!(context.build());
-        let contract = Contract::new_default_meta(accounts(1).into());
+        let contract = Contract::new(accounts(1).into(), nft_contract_metadata());
         testing_env!(context.is_view(true).build());
         assert_eq!(contract.nft_token("1".to_string()), None);
     }
@@ -206,7 +193,7 @@ mod tests {
     fn test_mint() {
         let mut context = get_context(accounts(0));
         testing_env!(context.build());
-        let mut contract = Contract::new_default_meta(accounts(0).into());
+        let mut contract = Contract::new(accounts(0).into(), nft_contract_metadata());
 
         testing_env!(context
             .storage_usage(env::storage_usage())
@@ -226,7 +213,7 @@ mod tests {
     fn test_transfer() {
         let mut context = get_context(accounts(0));
         testing_env!(context.build());
-        let mut contract = Contract::new_default_meta(accounts(0).into());
+        let mut contract = Contract::new(accounts(0).into(), nft_contract_metadata());
 
         testing_env!(context
             .storage_usage(env::storage_usage())
@@ -263,7 +250,7 @@ mod tests {
     fn test_approve() {
         let mut context = get_context(accounts(0));
         testing_env!(context.build());
-        let mut contract = Contract::new_default_meta(accounts(0).into());
+        let mut contract = Contract::new(accounts(0).into(), nft_contract_metadata());
 
         testing_env!(context
             .storage_usage(env::storage_usage())
@@ -294,7 +281,7 @@ mod tests {
     fn test_revoke() {
         let mut context = get_context(accounts(0));
         testing_env!(context.build());
-        let mut contract = Contract::new_default_meta(accounts(0).into());
+        let mut contract = Contract::new(accounts(0).into(), nft_contract_metadata());
 
         testing_env!(context
             .storage_usage(env::storage_usage())
@@ -332,7 +319,7 @@ mod tests {
     fn test_revoke_all() {
         let mut context = get_context(accounts(0));
         testing_env!(context.build());
-        let mut contract = Contract::new_default_meta(accounts(0).into());
+        let mut contract = Contract::new(accounts(0).into(), nft_contract_metadata());
 
         testing_env!(context
             .storage_usage(env::storage_usage())
